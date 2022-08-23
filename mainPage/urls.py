3eb600e3b83fc694b33bb3ed611814b1d50be1a8
ClearSky <ggogono2@gmail.com>
2022-08-23 22:52:37 +0900

    sakura effect

--- mainPage/urls.py
+++ mainPage/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.home, name='home'),
+    path('god', views.home),
+    path('card', views.home),
+    path('partner', views.home),
+    path('trio', views.home),
+    path('dual', views.home),
+
 ]
--- mainPage/views.py
+++ mainPage/views.py
@@ -1,3 +1,13 @@
+from itertools import repeat
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("메인 페이지 개발 중")
+
+def home(request):
+    return render(request, 'home.html')
